12def4ea4f553c7b8103dfd741e6abdeffb6f548
Ivan Fabriano Syahputra <78840337+ivanfabriano@users.noreply.github.com>
2024-02-28 14:57:33 +0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/ivanfabriano/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
@@ -13,7 +13,9 @@ job('NodeJS Docker example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
+        shell('echo "Attempting to build and publish the Docker image"')
         dockerBuildAndPublish {
+            registry('https://index.docker.io/v1/')
             repositoryName('ivanfabs/docker-jenkins')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
